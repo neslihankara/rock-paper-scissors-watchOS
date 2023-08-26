c84539b3463cef52abab75d1ed37632cacca273c
neslihankara <neslihankara40@gmail.com>
2023-08-26 17:46:30 +0300

    Initial Commit

---
+++ rps Watch App/rpsApp.swift
@@ -0,0 +1,17 @@
+//
+//  rpsApp.swift
+//  rps Watch App
+//
+//  Created by Neslihan Kara on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct rps_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
