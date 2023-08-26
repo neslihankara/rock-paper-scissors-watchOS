0aa99b29267a6a720b621b12281812428ef8c397
neslihankara <neslihankara40@gmail.com>
2023-08-26 18:57:44 +0300

    Implement the game logic

--- rps Watch App/ContentView.swift
+++ rps Watch App/ContentView.swift
@@ -1,21 +1,69 @@
 //
 //  ContentView.swift
-//  rps Watch App
+//  rock-paper-scissors Watch App
 //
 //  Created by Neslihan Kara on 26.08.2023.
 //
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+enum RockPaperScissors: String, CaseIterable {
+    case rock = "🪨 Rock"
+    case paper = "🧻 Paper"
+    case scissors = "✂️ Scissors"
+    
+    func beats(_ opponent: RockPaperScissors) -> Bool {
+            switch self {
+            case .rock:
+                return opponent == .scissors
+            case .paper:
+                return opponent == .rock
+            case .scissors:
+                return opponent == .paper
+            }
         }
-        .padding()
+}
+
+struct ContentView: View {
+    @State private var selectedOption: RockPaperScissors? = nil
+    @State private var opponentOption: RockPaperScissors? = nil
+    
+    // Sample data for demonstration
+    let options: [RockPaperScissors] = [.rock, .paper, .scissors]
+    
+    func handleTap(option: RockPaperScissors) {
+        selectedOption=option
+        opponentOption=options.randomElement()
+    }
+    
+    var body: some View {
+        VStack { if selectedOption != nil {
+            Text("You: " + selectedOption!.rawValue.split(separator: " ")[0]).font(.title3)
+            Text("Opponent: " + opponentOption!.rawValue.split(separator: " ")[0]).font(.title3)
+            if (selectedOption == opponentOption) {
+                Text("THAT'S A THAI 🇹🇭").padding(10)
+            } else if (selectedOption!.beats(opponentOption!)) {
+                Text("YOU WIN!!").padding(10)
+            } else {
+                Text("YOU LOSE...").padding(10)
+            }
+            Button(action: {
+                selectedOption = nil
+            }){Text("Go Back")}
+            }
+            
+          else {
+            List {
+                ForEach(options, id: \.self) { option in
+                    Button(action: {
+                        handleTap(option: option)
+                    }) {
+                        Text(option.rawValue)
+                    }
+                }
+            }
+        }
+        }
     }
 }
 
